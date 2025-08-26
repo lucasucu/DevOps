c92ce3a72f2e3f1861b9d7e6cb52821a3c88b827
Lobiwankinobi <lcaa348@gmail.com>
2025-08-26 17:44:20 -0300

    CSS & mais operações
    Demais operações como subtração, divisão e multiplicação foram adicionados.
    Estilo da calculadora criado.

--- calculadora.js
+++ calculadora.js
@@ -4,3 +4,25 @@ function somar() {
   const res = n1 + n2;
   document.getElementById("resultado").innerText = "Resultado: " + res;
 }
+function subtrair() {
+  const n1 = parseFloat(document.getElementById("num1").value);
+  const n2 = parseFloat(document.getElementById("num2").value);
+  const res = n1 - n2;
+  document.getElementById("resultado").innerText = "Resultado: " + res;
+}
+function multiplicar() {
+  const n1 = parseFloat(document.getElementById("num1").value);
+  const n2 = parseFloat(document.getElementById("num2").value);
+  const res = n1 * n2;
+  document.getElementById("resultado").innerText = "Resultado: " + res;
+}
+function dividir() {
+  const n1 = parseFloat(document.getElementById("num1").value);
+  const n2 = parseFloat(document.getElementById("num2").value);
+  if (n2 === 0) {
+    document.getElementById("resultado").innerText = "Erro: divisão por zero!";
+  } else {
+    const res = n1 / n2;
+    document.getElementById("resultado").innerText = "Resultado: " + res;
+  }
+}
